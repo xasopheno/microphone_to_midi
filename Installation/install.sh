02f373bae88d4c223a4f9791c918519cd9b6dd89
rahulremanan <rahul@remanan.net>
2018-03-28 00:03:47 -0400

    Added installation scripts

---
+++ Installation/install.sh
@@ -0,0 +1,14 @@
+﻿sudo apt-get install -y cmake git libasound2-dev python3-pip
+sudo pip3 install -r ./requirements.txt
+mkdir ../.tmp
+cd ../.tmp
+wget http://www.portaudio.com/archives/pa_stable_v190600_20161030.tgz
+tar -xvzf pa_stable_v190600_20161030.tgz
+cd ./portaudio
+./configure
+sudo make
+sudo make install
+sudo ldconfig
+cd ../
+sudo rm -r ./.tmp
+sudo python3 Record.py
