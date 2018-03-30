d817887c97f884bfbae55c96fc717206524d433b
rahulremanan <rahul@remanan.net>
2018-03-30 15:30:38 -0400

    Added install instructions

--- Installation/install.sh
+++ Installation/install.sh
@@ -8,6 +8,15 @@ cd ./portaudio
 make
 make install
 ldconfig
+cd ../
+wget https://aubio.org/pub/aubio-0.4.6.tar.bz2
+tar xf aubio-0.4.6.tar.bz2
+cd aubio-0.4.6
+./waf configure build
+./waf install
+python3 setup.py build
+python3 setup.py install
+ldconfig
 cd ../../Installation/
 pip3 install -r ./Requirements.txt
 rm -r ../.tmp
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# microphone_to_midi
+# Python package to convert microphone sounds to MIDI notes:
+
+* Installation: 
+	1) git clone https://github.com/xasopheno/microphone_to_midi
+	2) cd ./microphone_to_midi/Installation/
+	3) sudo chmod a+x ./install.sh
+	4) sudo ./install.sh
+* Running the program:
+	sudo python3 Record.py
