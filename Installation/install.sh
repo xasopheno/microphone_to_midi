81e5fafb118d3d99f9e8136c0902f5ddf9f28aab
rahulremanan <rahul@remanan.net>
2018-03-30 00:02:59 -0400

    Updated installer file

--- Installation/install.sh
+++ Installation/install.sh
@@ -1,14 +1,14 @@
-﻿sudo apt-get install -y cmake git libasound2-dev python3-pip
-sudo pip3 install -r ./Requirements.txt
+﻿apt-get install -y cmake git libasound2-dev python3-pip
 mkdir ../.tmp
 cd ../.tmp
 wget http://www.portaudio.com/archives/pa_stable_v190600_20161030.tgz
 tar -xvzf pa_stable_v190600_20161030.tgz
 cd ./portaudio
 ./configure
-sudo make
-sudo make install
-sudo ldconfig
+make
+make install
+ldconfig
+pip3 install -r ./Requirements.txt
 cd ../
-sudo rm -r ./.tmp
-sudo python3 Record.py
+rm -r ./.tmp
+python3 Record.py
